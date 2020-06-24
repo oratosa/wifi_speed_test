0615a417851ef811d33c2aa8860e5c04f8c76704
c121034t@icloud.com <c121034t@icloud.com>
2020-06-24 17:48:36 +0900

    speed testの結果を取得し，出力する

---
+++ speed_test.py
@@ -0,0 +1,15 @@
+import pprint
+import subprocess
+import json
+
+def get_speed_test_result():
+    process = subprocess.run(['speedtest','--json'], capture_output=True)
+    return json.loads(process.stdout)
+
+def job():
+    result = get_speed_test_result()
+    pprint.pprint(result)
+
+job()
+
+
