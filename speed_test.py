a4d066e8d63df9357048530edf0b146a742132e9
c121034t@icloud.com <c121034t@icloud.com>
2020-06-24 19:13:31 +0900

    実行時間も出力

--- speed_test.py
+++ speed_test.py
@@ -2,15 +2,20 @@ import pprint
 import subprocess
 import json
 import csv
+import datetime
 
 def get_speed_test_result():
     process = subprocess.run(['speedtest','--json'], capture_output=True)
     return json.loads(process.stdout)
 
+def bit_to_mbit(bit):
+    return bit/1024/1024
+
 def job():
     result = get_speed_test_result()
+    dt = datetime.datetime.now()
     with open('logs.csv', 'a') as f:
         writer = csv.writer(f)
-        writer.writerow([result['download'],result['upload'],result['client']['isp'], result['server']['host']])
+        writer.writerow([dt.strftime('%Y-%m-%d %H:%M:%S'),bit_to_mbit(result['download']),bit_to_mbit(result['upload']),result['client']['isp'], result['server']['host']])
 
 job()
